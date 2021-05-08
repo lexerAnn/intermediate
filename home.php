d2effdc7959662f7d51312a834249ed26d689ba0
lexerAnn <52282400+lexerAnn@users.noreply.github.com>
2021-05-08 22:11:11 +0000

    Add files via upload

---
+++ home.php
@@ -0,0 +1,66 @@
+<?php 
+ session_start();
+    session_destroy();
+ 
+?>
+<html lang="en">
+
+<!-- Mirrored from codervent.com/rocker/white-version/ by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 28 Oct 2018 14:41:38 GMT -->
+<head>
+  <meta charset="utf-8"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+  <meta name="description" content=""/>
+  <meta name="author" content=""/>
+  <title>NCA</title>
+  
+  <script   src="https://code.jquery.com/jquery-3.1.1.min.js"   integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="   crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+
+  <form>
+    <label for="fname">First name:</label><br>
+    <input type="text" id="fname" name="fname"><br>
+    <label for="lname">Last name:</label><br>
+    <input type="text" id="lname" name="lname">
+    <h3 id="channel"></h3><br>
+    <h3 id="frequency"></h3><br>
+ 
+    <button id="next-1" class="next">
+      <a href="#" class=" next" id="next-1" style="color: white">Next</a></button>
+  </form>
+
+  <script>
+ $(document).ready(function(){
+  $("#next-1").click(function(e){
+    e.preventDefault();
+    var details={
+      deviceId:$("#fname").val()
+    }
+    $.ajax({
+		url:('http://127.0.0.1:8000/api/spectrum_use'),
+		method:'post',
+		dataType: 'json',
+		data:details,
+		    success:function(response){	
+          console.log(response.databaseChange.channel) 
+          $('#channel').html(response.databaseChange.channel);
+          $('#frequency').html(response.databaseChange.frequency);
+		    },error:function(jqXhr, textStatus, errorThrown){
+						 console.log(errorThrown)
+					 }
+	});
+   
+  });
+});
+
+
+
+  </script>
+  
+</body>
+
+<!-- Mirrored from codervent.com/rocker/white-version/ by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 28 Oct 2018 14:43:13 GMT -->
+</html>
